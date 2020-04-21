ff9544fcb1ae703c0869018ce52ac5a84be27d74
Justin Ochoa <just.och92@gmail.com>
2020-04-21 09:05:21 -0400

    first 2 iterations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './navbar/Navbar.js';
+import formfield from './formfield/FormField.js'
+import FormField from './formfield/FormField.js';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+     <FormField/>
     </div>
   );
 }
